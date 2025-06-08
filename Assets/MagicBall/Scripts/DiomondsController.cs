6d59249aa7e8cd907a7ac15c36cd44b51cedd102
PavelPeng7 <104588369+PavelPeng7@users.noreply.github.com>
2025-06-08 17:01:53 +0800

    add: Magic Ball

--- Assets/MagicBall/Scripts/DiomondsController.cs
+++ Assets/MagicBall/Scripts/DiomondsController.cs
@@ -1,41 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Serialization;
 
 public class DiomondsController : MonoBehaviour
 {
-    #region ComputeShader相关属性
+    #region ComputeShader Properties
         public ComputeShader shader;
         public int texResolution = 1024;
         public Color clearColor = new Color(); 
-        public Color circleColor = new Color(); 
+        public Color diomondsColor = new Color(); 
         public int count = 10;
+        struct Diomonds
+        {
+            public Vector2 origin;
+            public Vector2 velocity;
+            public float radius;
+        }
+        Diomonds[] diomondsData; 
     #endregion
-    
-    Renderer rend;
-    RenderTexture outputTexture; 
 
-    int circlesHandle; 
-    int clearHandle;
-    
-    ComputeBuffer buffer;
-    
-    struct Circle
-    {
-        public Vector2 origin;
-        public Vector2 velocity;
-        public float radius;
-    }
-    Circle[] circleData; 
-    
-    
+    #region Runtime Properties
+        Renderer rend;
+        RenderTexture outputTexture; 
 
-    #region 周期
+        int diomondsHandle; 
+        int clearHandle;
+        
+        ComputeBuffer buffer;
+    #endregion
+
+    #region Core Methods
         void Start()
         {
             outputTexture = new RenderTexture(texResolution, texResolution, 0);
             outputTexture.enableRandomWrite = true;
-            // outputTexture.useMipMap = true;
-            // outputTexture.autoGenerateMips = true;
             outputTexture.wrapMode = TextureWrapMode.Repeat;
             outputTexture.Create();
 
@@ -57,33 +55,33 @@ public class DiomondsController : MonoBehaviour
         }
     #endregion
 
-    #region MyRegion
+    #region Compute Shader Methods
         private void InitData()
         {
-            circlesHandle = shader.FindKernel("Circles");
+            diomondsHandle = shader.FindKernel("Diomonds");
 
             uint threadGroupSizeX;
             
-            shader.GetKernelThreadGroupSizes(circlesHandle, out threadGroupSizeX, out _, out _);
+            shader.GetKernelThreadGroupSizes(diomondsHandle, out threadGroupSizeX, out _, out _);
 
             int total = (int)threadGroupSizeX * count;
-            circleData = new Circle[total];
+            diomondsData = new Diomonds[total];
 
             float speed = 100;
             float halfSpeed = speed * 0.5f;
             float minRadius = 5.0f;
-            float maxRadius = 15.0f;
+            float maxRadius = 10.0f;
             float radiusRange = maxRadius - minRadius;
 
             for (int i = 0; i < total; i++)
             {
-                Circle circle = circleData[i];
-                circle.origin.x = Random.value * texResolution; 
-                circle.origin.y = Random.value * texResolution; 
-                circle.velocity.x = (Random.value * speed) - halfSpeed;
-                circle.velocity.y = (Random.value * speed) - halfSpeed;
-                circle.radius = Random.value * radiusRange + minRadius;
-                circleData[i] = circle; 
+                Diomonds diomonds = diomondsData[i];
+                diomonds.origin.x = Random.value * texResolution; 
+                diomonds.origin.y = Random.value * texResolution; 
+                diomonds.velocity.x = (Random.value * speed) - halfSpeed;
+                diomonds.velocity.y = (Random.value * speed) - halfSpeed;
+                diomonds.radius = Random.value * radiusRange + minRadius;
+                diomondsData[i] = diomonds; 
             }
         }
         
@@ -92,16 +90,15 @@ public class DiomondsController : MonoBehaviour
             clearHandle = shader.FindKernel("Clear");
             
             shader.SetVector("clearColor", clearColor);
-            shader.SetVector("circleColor", circleColor);
+            shader.SetVector("diomondsColor", diomondsColor);
             shader.SetInt("texResolution", texResolution);
-
-
+            
             int stride = (2 + 2 + 1) * 4;
-            buffer = new ComputeBuffer(circleData.Length, stride);
-            buffer.SetData(circleData); 
-            shader.SetBuffer(circlesHandle, "circlesBuffer", buffer);
+            buffer = new ComputeBuffer(diomondsData.Length, stride);
+            buffer.SetData(diomondsData); 
+            shader.SetBuffer(diomondsHandle, "diomondsBuffer", buffer);
 
-            shader.SetTexture(circlesHandle, "Result", outputTexture);
+            shader.SetTexture(diomondsHandle, "Result", outputTexture);
             shader.SetTexture(clearHandle, "Result", outputTexture);
 
             rend.material.SetTexture("_MainTex", outputTexture);
@@ -111,12 +108,9 @@ public class DiomondsController : MonoBehaviour
         {
             shader.Dispatch(clearHandle, texResolution / 8, texResolution / 8, 1);
             shader.SetFloat("time", Time.time);
-            shader.Dispatch(circlesHandle, count, 1, 1);
+            shader.Dispatch(diomondsHandle, count, 1, 1);
         }
     
 
     #endregion
-
-
-
 }
